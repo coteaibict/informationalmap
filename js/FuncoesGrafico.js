abd853dc3c7202a8865740f426d135f849e69759
rafalopes888 <rafalopes88@gmail.com>
2017-09-13 15:56:26 -0300

    resolução de varios pequenos problemas

--- js/FuncoesGrafico.js
+++ js/FuncoesGrafico.js
@@ -92,6 +92,7 @@ function GraficoDespesas(cod){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
+      console.log(this.responseText);
       var dados = this.responseText.split(";");
       var nomes = [];
       var valores = [];
