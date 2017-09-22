cbcbad1582c5bacd5ba69ad4872037f5724cf481
LucasVF <lucas_v.f@hotmail.com>
2017-09-22 07:35:20 -0300

    Update - Correcao de Bug

--- php/GraficoHistorico.php
+++ php/GraficoHistorico.php
@@ -8,20 +8,23 @@
 							from pibmunicipalibge pm inner join municipio m on pm.cod_municipio = m.cod_municipio 				
 							where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
                             group by pm.cod_ano;");
-		}if($_GET["tipo"] == "M"){
+
+		}else if($_GET["tipoDivisao"] == "M"){
 			$res = $con->query("SELECT pm.PIB ,pm.cod_ano +2001
 							from pibmunicipalibge pm inner join municipio m on pm.cod_municipio = m.cod_municipio 				
 							where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 							and m.cod_municipio =".$_GET["cod"]."
                             group by pm.cod_ano;");
-		}if($_GET["tipo"] == "MR"){
+
+		}else if($_GET["tipoDivisao"] == "MR"){
 			$res = $con->query("SELECT sum(pm.PIB) ,pm.cod_ano +2001
 							from pibmunicipalibge pm inner join municipio m on pm.cod_municipio = m.cod_municipio
 							inner join mesoregiao e on m.cod_mesoregiao = e.cod_mesoregiao				
 							where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 							and m.cod_mesoregiao =".$_GET["cod"]."
                             group by pm.cod_ano;");
-		}if($_GET["tipo"] == "E"){
+
+		}else if($_GET["tipoDivisao"] == "E"){
 			$res = $con->query("SELECT sum(pm.PIB) ,pm.cod_ano +2001
 							from pibmunicipalibge pm inner join municipio m on pm.cod_municipio = m.cod_municipio
 							inner join estado e on m.cod_estado = e.cod_estado				
