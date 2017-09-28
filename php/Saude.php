dd5f3cc698121074c14a9a64fffb0511eb9c25a2
LucasVF <lucas_v.f@hotmail.com>
2017-09-28 11:16:46 -0300

    Update - Saude com Anos

--- php/Saude.php
+++ php/Saude.php
@@ -2,51 +2,57 @@
 	function QntMedicos(){
 		include "ConexaoDB.php";
 		if($_GET["tipo"] == "M"){
-			$res = $con->query("SELECT m.cod_municipio, '',  dm.valor, m.nome, null
+			$res = $con->query("SELECT m.cod_municipio,  a.descricao,  dm.valor, m.nome, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Quantidade de médicos (*) '
-			                    group by m.cod_municipio
+			                    group by m.cod_municipio, a.cod_ano
 			                    order by m.cod_municipio");
 			
-			$resd = $con->query("SELECT valor 
+			$resd = $con->query("SELECT dm.valor 
 								from variaveisdobim dm
-								where informacao = 'Recursos humanos da área da saúde - Total(*) '
-                                group by cod_municipio
-								order by cod_municipio");
+								inner join ano a on a.cod_ano = dm.cod_ano
+								where dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
+                                group by dm.cod_municipio, a.cod_ano
+								order by dm.cod_municipio");
 		}
 		else if($_GET["tipo"] == "E"){
-			$res = $con->query("SELECT m.cod_estado, '',  sum(dm.valor), e.uf, null
+			$res = $con->query("SELECT m.cod_estado,  a.descricao,  sum(dm.valor), e.uf, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
 				                inner join estado e on e.cod_estado = m.cod_estado
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Quantidade de médicos (*) '
-			                    group by m.cod_estado
+			                    group by m.cod_estado, a.cod_ano
 			                    order by m.cod_municipio");
 			
 			$resd = $con->query("SELECT sum(dm.valor) 
 								from variaveisdobim dm 
 						        inner join municipio m on m.cod_municipio = dm.cod_municipio
 						        inner join estado e on e.cod_estado = m.cod_estado
+						        inner join ano a on a.cod_ano = dm.cod_ano
 								where dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
-						        group by m.cod_estado
+						        group by m.cod_estado, a.cod_ano
 								order by m.cod_municipio");
 		}
 		else if($_GET["tipo"] == "MR"){
-			$res = $con->query("SELECT m.cod_mesoRegiao, '',  sum(dm.valor), e.nome, null
+			$res = $con->query("SELECT m.cod_mesoRegiao,  a.descricao,  sum(dm.valor), e.nome, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
 				                inner join mesoRegiao e on e.cod_mesoRegiao = m.cod_mesoRegiao
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Quantidade de médicos (*) '
-			                    group by m.cod_mesoRegiao
+			                    group by m.cod_mesoRegiao, a.cod_ano
 			                    order by m.cod_municipio");
 			
 			$resd = $con->query("SELECT sum(dm.valor) 
 								from variaveisdobim dm 
 						        inner join municipio m on m.cod_municipio = dm.cod_municipio
 						        inner join mesoRegiao e on e.cod_mesoRegiao = m.cod_mesoRegiao
+						        inner join ano a on a.cod_ano = dm.cod_ano
 								where dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
-						        group by m.cod_mesoRegiao
+						        group by m.cod_mesoRegiao, a.cod_ano
 								order by m.cod_municipio");
 		}
 
@@ -61,29 +67,32 @@
 	function RecursosSaude(){
 		include "ConexaoDB.php";
 		if($_GET["tipo"] == "M"){
-			$res = $con->query("SELECT m.cod_municipio, '',  dm.valor, m.nome, null
+			$res = $con->query("SELECT m.cod_municipio,  a.descricao,  dm.valor, m.nome, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
-			                    group by m.cod_municipio
+			                    group by m.cod_municipio, a.cod_ano
 			                    order by m.cod_municipio");
 		}
 		else if($_GET["tipo"] == "E"){
-			$res = $con->query("SELECT m.cod_estado, '',  sum(dm.valor), e.uf, null
+			$res = $con->query("SELECT m.cod_estado,  a.descricao,  sum(dm.valor), e.uf, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
 				                inner join estado e on e.cod_estado = m.cod_estado
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
-			                    group by m.cod_estado
+			                    group by m.cod_estado, a.cod_ano
 			                    order by m.cod_municipio");
 		}
 		else if($_GET["tipo"] == "MR"){
-			$res = $con->query("SELECT m.cod_mesoRegiao, '',  sum(dm.valor), e.nome, null
+			$res = $con->query("SELECT m.cod_mesoRegiao,  a.descricao,  sum(dm.valor), e.nome, null
 				                from  municipio m inner join variaveisdobim dm on m.cod_municipio = dm.cod_municipio
 				                inner join mesoRegiao e on e.cod_mesoRegiao = m.cod_mesoRegiao
+				                inner join ano a on a.cod_ano = dm.cod_ano
 								where m.cod_municipio not in (2206720, 1504752,4212650, 4220000, 4314548, 5006275)
 			                    and dm.informacao = 'Recursos humanos da área da saúde - Total(*) '
-			                    group by m.cod_mesoRegiao
+			                    group by m.cod_mesoRegiao, a.cod_ano
 			                    order by m.cod_municipio");
 		}
 
